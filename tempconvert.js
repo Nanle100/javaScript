4944e18e18174f9e53a188bfb4392fa9e62e9b0b
nathaniel <nathaielnanle100@gmail.com>
2024-03-07 19:04:18 +0100

    Temperature conversion

---
+++ tempconvert.js
@@ -0,0 +1,14 @@
+ const Kelvin = 0;
+ // 293 is assign to the variable//
+ const celsius = Kelvin - 273;
+ // 20 is assign to the variable celsius //
+let fahrenheit = celsius * (9 / 5) + 32;
+// The values in the variable can be changed
+fahrenheit = Math.floor(fahrenheit);
+// values are round down //
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+
+
+Newton = celsius * (33/100)
+Newton = Math.floor(Newton);
+console.log(`The temperature is ${Newton} is Newton.`)
