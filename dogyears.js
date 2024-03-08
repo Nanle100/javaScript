098fda5b04c0e03789dd50b6710abf132f069e88
nathaniel <nathaielnanle100@gmail.com>
2024-03-08 17:56:30 +0100

    conversion from human years to Dog years

---
+++ dogyears.js
@@ -0,0 +1,15 @@
+// my age
+const myAge = 28;
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+let laterYears = myAge - 2;
+laterYears *= 4;
+console.log(earlyYears);
+console.log(laterYears);
+
+const myAgeInDogYears = earlyYears + laterYears
+const myName = 'Nanle Nathaniel'.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+
