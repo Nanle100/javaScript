e0c73c1dae3c5d24e3223c6bc993be5fa27abff3
nathaniel <nathaielnanle100@gmail.com>
2024-03-25 14:16:56 +0100

    understanding blocks and scope

---
+++ blockscope.js
@@ -0,0 +1,42 @@
+
+// // const color = 'brown';
+// // const skyWalker = () => {
+// //     return color;
+// // }
+// // let colorOne = skyWalker();
+// // console.log(colorOne)
+// // /
+// // const colorDream = ' Pink';
+// // function screen() {
+// //     let color = colorDream;
+// //     return color;
+// // }
+// // let choose = screen();
+// // console.log(choose);
+// // console.log(colorDream);
+
+// // const promise = () => {
+// //     let pride = 'No promise';
+// //     // console.log(pride);
+// //     return pride;
+// // }
+// // console.log(promise());
+
+// const logSkyColor = () => {
+//     let color = 'blue'; 
+//     console.log(color); 
+//   };
+  
+//   console.log(logSkyColor());
+
+
+let abc = 'first';
+let def = 'second';
+let ghi = 'third';
+
+const alphabet = () => {
+    abc = 'fourth'
+    return 'you can only come as ' + abc + ',' + def + ',and ' + ghi 
+}
+console.log(alphabet());
+console.log(abc)
